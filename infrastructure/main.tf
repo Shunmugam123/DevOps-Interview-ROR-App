e87fda956dfbad4d25172ace2f51e21945a64ad2
Shunmugam B <anishknsk@gmail.com>
2023-10-14 19:55:49 +0530

    Terraform files

---
+++ infrastructure/main.tf
@@ -0,0 +1,30 @@
+terraform {
+    required_version = "~> 1.3"
+
+    backend "s3" {
+        bucket         = "cc-tf-demo"
+        key            = "tf-infra/terraform.tfstate"
+        region         = "eu-north-1"
+        dynamodb_table = "ccTfDemo"
+        encrypt        = true
+    }
+
+    required_providers {
+      aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+      }
+    }
+}
+
+module "tf_state" {
+    source = "./modules/tf-state"
+    bucket_name = local.bucket_name
+    table_name = local.table_name
+}
+
+module "ecrRepo" {
+    source = "./modules/ecr"
+
+    ecr_repo_name = local.ecr_repo_name
+}
