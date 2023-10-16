89e4ba22118f44b9eb05fe0146b7a6081312f491
root <root@ip-172-31-31-247.eu-north-1.compute.internal>
2023-10-16 02:48:11 +0000

    Backend

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -5,7 +5,6 @@ backend "s3" {
         bucket         = "s3-ror"
         key            = "tf-infra/terraform.tfstate"
         region         = "eu-central-1"
-        table_name     = "tableRoR"
         encrypt        = true
   }
   required_providers {
