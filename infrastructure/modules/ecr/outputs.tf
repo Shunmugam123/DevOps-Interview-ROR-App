31fcdc650ab2a9a09de8a4d22221b85b23d7de98
Shunmugam B <anishknsk@gmail.com>
2023-10-16 08:57:09 +0530

    Output changes

--- infrastructure/modules/ecr/outputs.tf
+++ infrastructure/modules/ecr/outputs.tf
@@ -2,14 +2,14 @@ output "repository_url" {
   value = aws_ecr_repository.ror_app_ecr_repo.repository_url
 }
 
-output "repository_url" {
+output "repository_url1" {
   value = aws_ecr_repository.ror_app_ecr_repo1.repository_url
 }
 
-output "repository_url" {
+output "repository_url2" {
   value = aws_ecr_repository.ror_app_ecr_repo2.repository_url
 }
 
-output "repository_url" {
+output "repository_url3" {
   value = aws_ecr_repository.ror_app_ecr_repo3.repository_url
 }
