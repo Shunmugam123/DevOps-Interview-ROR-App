81cb668b406b7af2d8db7cfd3ad786311564968c
Shunmugam B <anishknsk@gmail.com>
2023-10-16 05:47:20 +0530

    IP

--- infrastructure/modules/tf-state/tf-state.tf
+++ infrastructure/modules/tf-state/tf-state.tf
@@ -31,7 +31,7 @@ resource "aws_subnet" "my-subnet1" {
 
 resource "aws_subnet" "my-subnet2" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "eu-central-1b"
 }
 
